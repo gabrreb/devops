9ac77ff666dabdfaa7d5e51601cb3c4e9e97e7b8
gabrreb <gabriela.obana@outlook.com>
2021-08-17 10:42:44 -0300

    Circle CI

---
+++ src/main/java/br/com/fabiogomesrocha/cursorest/CursoRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabiogomesrocha.cursorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fabiogomesrocha/cursorest/CursoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiogomesrocha.cursorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
